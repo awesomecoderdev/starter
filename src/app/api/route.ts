37143005432cd08624b5bfdb6bab46659773ecc5
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-05-15 20:01:54 +0600

    Updated Prisma

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,5 @@
-import { MethodNotALlowed } from "@/lib/http";
 import prisma from "@/prisma/client";
-import Status from "@/utils/http";
+import Status, { MethodNotALlowed } from "@/utils/http";
 import { cookies } from "next/headers";
 
 interface Data {
@@ -8,11 +7,8 @@ interface Data {
 }
 
 export async function GET(request: Request) {
-	// const query = request?.nextUrl?.searchParams;
-	const cookieStore = cookies();
-	const token = cookieStore.get("token");
+	const cookie = cookies();
 
-	// console.log({ slug, params, query, token: token?.value });
 	// const req = await fetch("https://jsonplaceholder.typicode.com/posts");
 	// const res = await req.json();
 
@@ -24,66 +20,61 @@ export async function GET(request: Request) {
 	// 	};
 	// });
 
-	// const user = await prisma.user.create({
-	// 	data: {
-	// 		name: "Md. Ibrahim Kholil",
-	// 		email: "awesomecoder.dev@gmail.com",
-	// 		password: "password",
-	// 		posts: {
-	// 			create: posts,
+	// return new Response(
+	// 	JSON.stringify({
+	// 		success: true,
+	// 		status: Status.HTTP_CREATED,
+	// 		data: {
+	// 			count: posts.length,
+	// 			posts,
 	// 		},
-	// 	},
-	// });
-	// console.log("user", user);
-	// const user = await prisma.user.findFirst();
+	// 	}),
+	// 	{
+	// 		status: Status.HTTP_CREATED,
+	// 	}
+	// );
 
-	// const posts = await prisma.post.create({
-	// 	data: {
-	// 		title: "Hello World",
-	// 		authorId: user.id,
-	// 	},
-	// });
-
-	const posts = await prisma.post.findMany({
-		// where: {
-		// 	id: {
-		// 		in: [1, 2],
-		// 	},
-		// },
-		select: {
-			id: true,
-			title: true,
-			author: {
-				select: {
-					name: true,
+	try {
+		const posts = await prisma.post.findMany({
+			select: {
+				id: true,
+				title: true,
+				author: {
+					select: {
+						name: true,
+					},
 				},
 			},
-		},
-		// include: {
-		// 	author: true,
-		// },
-		// cursor: { id: 5 },
-		take: 10,
-	});
+			take: 10,
+		});
 
-	return new Response(
-		JSON.stringify({
-			success: true,
-			status: Status.HTTP_CREATED,
-			data: {
-				count: posts.length,
-				posts,
-			},
-		}),
-		{
-			status: Status.HTTP_CREATED,
-			// headers: {
-			// 	"Set-Cookie": `token=${btoa(JSON.stringify(posts))},count=${
-			// 		posts.length
-			// 	}`,
-			// },
-		}
-	);
+		return new Response(
+			JSON.stringify({
+				success: true,
+				status: Status.HTTP_CREATED,
+				data: {
+					count: posts.length,
+					posts,
+				},
+			}),
+			{
+				status: Status.HTTP_CREATED,
+			}
+		);
+	} catch (error) {
+		return new Response(
+			JSON.stringify({
+				success: false,
+				status: Status.HTTP_BAD_REQUEST,
+				data: {
+					error,
+				},
+			}),
+			{
+				status: Status.HTTP_BAD_REQUEST,
+			}
+		);
+	}
 }
 
 export {
--- src/prisma/client.ts
+++ src/prisma/client.ts
@@ -1,43 +1,43 @@
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-	namespace NodeJS {
-		interface Global {}
-	}
-}
-
-// add prisma to the NodeJS global type
-interface CustomNodeJsGlobal extends NodeJS.Global {
-	prisma: PrismaClient;
-}
-
-// Prevent multiple instances of Prisma Client in development
-declare const global: CustomNodeJsGlobal;
-
-const prisma = global.prisma || new PrismaClient();
-if (process.env.NODE_ENV === "development") global.prisma = prisma;
-
-// prisma.$use(async (params: any, next: any) => {
-// 	if (params.model === "User") {
-// 		if (params.action === "create" || params.action === "update") {
-// 			if (params.args.data.password) {
-// 				const hashPass = btoa(`${params.args.data.password}`);
-// 				params.args.data.password = hashPass;
-// 			}
-// 		}
-// 	}
-// 	return next(params);
-// });
-
-export default prisma;
-
 // import { PrismaClient } from "@prisma/client";
 
 // declare global {
-//   var prisma: PrismaClient | undefined;
+// 	namespace NodeJS {
+// 		interface Global {}
+// 	}
 // }
 
-// const client = globalThis.prisma || new PrismaClient();
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+// // add prisma to the NodeJS global type
+// interface CustomNodeJsGlobal extends NodeJS.Global {
+// 	prisma: PrismaClient;
+// }
 
-// export default client;
+// // Prevent multiple instances of Prisma Client in development
+// declare const global: CustomNodeJsGlobal;
+
+// const prisma = global.prisma || new PrismaClient();
+// if (process.env.NODE_ENV === "development") global.prisma = prisma;
+
+// // prisma.$use(async (params: any, next: any) => {
+// // 	if (params.model === "User") {
+// // 		if (params.action === "create" || params.action === "update") {
+// // 			if (params.args.data.password) {
+// // 				const hashPass = btoa(`${params.args.data.password}`);
+// // 				params.args.data.password = hashPass;
+// // 			}
+// // 		}
+// // 	}
+// // 	return next(params);
+// // });
+
+// export default prisma;
+
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+	var prisma: PrismaClient | undefined;
+}
+
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export default client;
