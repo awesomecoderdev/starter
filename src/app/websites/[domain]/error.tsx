f3d1336a2d533d09b2b97a154cfe227d0a8328ac
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-05-29 22:27:31 +0600

    Updated Bradgrambs

--- src/app/websites/[domain]/error.tsx
+++ src/app/websites/[domain]/error.tsx
@@ -1,8 +1,30 @@
-"use client";
-import React from "react";
+"use client"; // Error components must be Client Components
+import BlurImage from "@/components/BlurImage";
+import { Prose } from "@/components/Prose";
+import { CallWaiting } from "@/components/icons/Static";
+import { useEffect } from "react";
 
-const DomainError = () => {
-	return <div>domain error</div>;
-};
+export default function DomainError({
+	error,
+	reset,
+}: {
+	error: Error;
+	reset: () => void;
+}) {
+	useEffect(() => {
+		// Log the error to an error reporting service
+		console.error(error);
+	}, [error]);
 
-export default DomainError;
+	return (
+		<Prose>
+			<div className="flex flex-col items-center justify-center rounded-md  py-12">
+				<h2 className="z-10 text-xl font-semibold ">Invalid Domain.</h2>
+				<CallWaiting className="pointer-events-none -my-8 w-96" />
+				{/* <AddEditLinkButton
+				/> */}
+				<p className="mt-2 text-sm ">or edit your search filters</p>
+			</div>
+		</Prose>
+	);
+}
